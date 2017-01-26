3e1a86168e52bfb2103ad8983f936ce2ce8c6413
Jessie Enero <jessie@lumikha.co>
2017-01-26 19:13:39 +0800

    added 404 redirections for each page

--- contact.php
+++ contact.php
@@ -1,4 +1,10 @@
 <?php include 'header.php'; ?>
+
+<?php 
+	header("Location: https://lumikha.co/404");
+	die();
+?>
+
 <center style="margin-top: 100px;">
 	<div class="on_construct_page_img">
 		<img src="img/page_is_under_construction.jpg">
